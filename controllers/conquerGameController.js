fb9aef5d3b3764c065a4e5f62ff31fa9783e4efc
Liuts213 <angel.espinozatorres213@gmail.com>
2023-01-05 17:05:58 -0800

    Se esta trabajando en la nueva xcondicion para el proceso de conquergame

--- controllers/conquerGameController.js
+++ controllers/conquerGameController.js
@@ -431,24 +431,30 @@ exports.actualizarPiezasPosicionJuego =  async (req,res) =>{
                 }
             }
         );
-        if(partida.tipoJuego === 1 && Object.keys(arrResultado).length == 1){
-            vActualizar.$set.estatus = 4
-            vActualizar.$set.ganador = arrResultado[0][0];
-            const usuario = await Usuario.findOne({'_id':Buffer.from(req.headers.authorization, 'base64').toString('ascii')});
-            vActualizar.$push = {}
-            vActualizar.$push.historialJugadores = "Victoria del jugador "+usuario.usuario;
-        }else if(partida.tipoJuego === 2 && 
+        if((partida.tipoJuego === 1 && Object.keys(arrResultado).length == 1) || 
+            (partida.tipoJuego === 2 && partida.cantidadJugadores === 4 &&
             //Si el tipo de partida es en equipo
             (( Object.keys(arrResultado).length == 2 &&
             //Si la partida se detecta que los ultimos dos valores son orange y black o red y purple entonces se decide el ganador 
-            ((arrResultado[0][0] === "O" && arrResultado[1][0] ==="B" || (arrResultado[0][0] === "R" && arrResultado[1][0] ==="P")))
+            ((arrResultado[0][0] === "O" && arrResultado[1][0] ==="B" 
+            || (arrResultado[0][0] === "R" && arrResultado[1][0] ==="P")))
             // se valida que solo halla un resultado en caso de que los jugadores que queden sean de diferentres equipos
-            || Object.keys(arrResultado).length === 1 ))){
+            || Object.keys(arrResultado).length === 1 ))) || 
+            (partida.tipoJuego === 2 && partida.cantidadJugadores === 6 &&
+            //Si el tipo de partida es en equipo
+            (( Object.keys(arrResultado).length == 3 &&
+            //Si la partida se detecta que los ultimos dos valores son orange y black o red y purple entonces se decide el ganador 
+            ((arrResultado[0][0] === "O" && arrResultado[1][0] ==="B" && arrResultado[2][0] ==="R" 
+            || (arrResultado[0][0] === "P" && arrResultado[1][0] ==="G" && arrResultado[2][0] ==="Y")))
+            // se valida que solo halla un resultado en caso de que los jugadores que queden sean de diferentres equipos
+            || Object.keys(arrResultado).length === 1 )))){
                 vActualizar.$set.estatus = 4
                 if ( Object.keys(arrResultado).length === 1){
                     vActualizar.$set.ganador = arrResultado[0][0];
-                }else{
+                }else if(Object.keys(arrResultado).length === 2){
                     vActualizar.$set.ganador = arrResultado[0][0] +" " +arrResultado[1][0];    
+                }else{
+                    vActualizar.$set.ganador = arrResultado[0][0] +" " +arrResultado[1][0] + " "+arrResultado[2][0];    
                 }
                 const usuario = await Usuario.findOne({'_id':Buffer.from(req.headers.authorization, 'base64').toString('ascii')});
                 vActualizar.$push = {}
